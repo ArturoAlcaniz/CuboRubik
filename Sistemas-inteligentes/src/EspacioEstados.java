368c0d15ef7d23ceb079dcf6d138d45ae214e0f3
ArturoAlcaniz <55736909+ArturoAlcaniz@users.noreply.github.com>
2019-11-13 15:54:32 +0100

    Update EspacioEstados.java

--- Sistemas-inteligentes/src/EspacioEstados.java
+++ Sistemas-inteligentes/src/EspacioEstados.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 
 public class EspacioEstados {
@@ -24,37 +25,55 @@ public class EspacioEstados {
 			String[] sucesor = {OperacionesCubo.convertircubomatriz(cubomatriz), "B"+i};
 			ListaSucesores.add(sucesor);
 			
-			cubomatriz = new ArrayList<String[][]>();
+		}
+		
+		for (int i = 0; i<N; i++) {
+
+			ArrayList<String[][]> cubomatriz = new ArrayList<String[][]>();
 			cubomatriz = OperacionesCubo.inicializarcubomatriz(cubomatriz, cubo);
 			OperacionesCubo.GirarBack2(cubomatriz, i);
-			String[] sucesor2 = {OperacionesCubo.convertircubomatriz(cubomatriz), "b"+i};
-			ListaSucesores.add(sucesor2);
-
-			cubomatriz = new ArrayList<String[][]>();
+			String[] sucesor = {OperacionesCubo.convertircubomatriz(cubomatriz), "b"+i};
+			ListaSucesores.add(sucesor);
+			
+		}
+		for(int i = 0; i<N; i++) {
+			ArrayList<String[][]> cubomatriz = new ArrayList<String[][]>();
 			cubomatriz = OperacionesCubo.inicializarcubomatriz(cubomatriz, cubo);
 			OperacionesCubo.GirarDown(cubomatriz, i);
-			String[] sucesor3 = {OperacionesCubo.convertircubomatriz(cubomatriz), "D"+i};
-			ListaSucesores.add(sucesor3);
-
-			cubomatriz = new ArrayList<String[][]>();
+			String[] sucesor = {OperacionesCubo.convertircubomatriz(cubomatriz), "D"+i};
+			ListaSucesores.add(sucesor);
+			
+		}
+		for(int i = 0; i<N; i++) {
+			ArrayList<String[][]> cubomatriz = new ArrayList<String[][]>();
 			cubomatriz = OperacionesCubo.inicializarcubomatriz(cubomatriz, cubo);
 			OperacionesCubo.GirarDown2(cubomatriz, i);
-			String[] sucesor4 = {OperacionesCubo.convertircubomatriz(cubomatriz), "d"+i};
-			ListaSucesores.add(sucesor4);
+			String[] sucesor = {OperacionesCubo.convertircubomatriz(cubomatriz), "d"+i};
+			ListaSucesores.add(sucesor);
 			
-			cubomatriz = new ArrayList<String[][]>();
+		}
+		for(int i = 0; i<N; i++) {
+			ArrayList<String[][]> cubomatriz = new ArrayList<String[][]>();
 			cubomatriz = OperacionesCubo.inicializarcubomatriz(cubomatriz, cubo);
 			OperacionesCubo.GirarLeft(cubomatriz, i);
-			String[] sucesor5 = {OperacionesCubo.convertircubomatriz(cubomatriz), "L"+i};
-			ListaSucesores.add(sucesor5);
+			String[] sucesor = {OperacionesCubo.convertircubomatriz(cubomatriz), "L"+i};
+			ListaSucesores.add(sucesor);
 			
-			cubomatriz = new ArrayList<String[][]>();
+		}
+		for(int i = 0; i<N; i++) {
+			ArrayList<String[][]> cubomatriz = new ArrayList<String[][]>();
 			cubomatriz = OperacionesCubo.inicializarcubomatriz(cubomatriz, cubo);
 			OperacionesCubo.GirarLeft2(cubomatriz, i);
-			String[] sucesor6 = {OperacionesCubo.convertircubomatriz(cubomatriz), "l"+i};
-			ListaSucesores.add(sucesor6);
+			String[] sucesor = {OperacionesCubo.convertircubomatriz(cubomatriz), "l"+i};
+			ListaSucesores.add(sucesor);
+			
 				
 		}
+		/*Iterator<String[]> it = ListaSucesores.iterator();
+		while(it.hasNext()){
+			String[] n = it.next();
+			System.out.println(" "+n[0]+" "+n[2]);
+		*/
 		return ListaSucesores;
 		
 	}
